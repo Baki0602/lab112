9892eaf1a086d1ab06d01ab499589ef139843462
Admin <Admin@DESKTOP-NVVN2EB>
2024-04-17 10:04:49 +0800

    Muugii

---
+++ lab12/src/lab12/Baki.java
@@ -0,0 +1,11 @@
+package lab12;
+
+public class Baki {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello my name is Muugii");
+
+	}
+
+}
